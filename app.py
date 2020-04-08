c1c489c95e474c3e81ecd98d36b5750b546f60ad
Divya Ann Kurien <divya.kurien@gmail.com>
2020-04-09 01:39:27 +0530

    Added todolist, also saved tasks to sqlalchemy

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask,render_template,request,redirect,url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app= Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////home/divya/ToDo_app/todo.db'
+
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200))
+    complete = db.Column(db.Boolean)
+
+@app.route('/')
+def index():
+    todos=Todo.query.all()
+    return render_template('index.html',todos=todos)
+
+@app.route('/add',methods=['POST'])
+def add():
+    todo = Todo(text=request.form['task'],complete=False)
+    db.session.add(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+if __name__=='__main__':
+    app.run(debug=True)
